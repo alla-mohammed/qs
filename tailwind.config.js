b8ea67f216e3c9476cf25121df1cd78a5ed3374a
alla-mohammed <155820419+alla-mohammed@users.noreply.github.com>
2026-07-02 19:17:47 +0300

    edited

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./docs/**/*.{html,js}",
-    "./js/**/*.js"
+    "./docs/**/*.{html,js}"
   ],
   theme: {
     extend: {},
